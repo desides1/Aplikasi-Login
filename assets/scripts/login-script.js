1bd242018fec67913fd44af5e243f4380dfa21ad
desides1 <121778860+desides1@users.noreply.github.com>
2023-08-18 09:40:27 +0700

    Update login-script.js
    menambahkan komen

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -4,22 +4,30 @@
  * Gunakan fungsi goToLogin() untuk menampilkan halaman login
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
-
+/* comment : membuat variabel untuk setiap elemen view. */
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/* comment :  membuat variabel untuk menyimpan informasi email dan password. */
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+/* comment : menambahkan aksi klik pada button */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  /* mendapatkan nilai masing-masing input(email dan password) */
   const email = inputEmailElement.value;
   const password = inputPasswordElement.value;
+
+  /* memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan */
   if (email == expectedEmail && password == expectedPassword) {
+
+    /* jika sesuai maka program akan berpindah ke halaman home. */
     goToHome();
   } else {
+    /* namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
     showPopUp();
   }
 });
